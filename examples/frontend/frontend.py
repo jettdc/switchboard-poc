876ec43f4a8cef15a90dd9c59c1d9f66b62f92da
Moppit <michelletranusa@gmail.com>
2022-11-22 13:41:44 -0700

    Created base template and dir structure

---
+++ examples/frontend/frontend.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, Response, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('base.html')
+
+@app.route("/login")
+def login():
+    pass
+
+@app.route("/order")
+def order():
+    pass
+
+@app.route("/track")
+def track():
+    return render_template('track.html')
+
+app.run(host="0.0.0.0", port=5000)
