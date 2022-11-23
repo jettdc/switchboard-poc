dcef06e15526ea2ce07b8190db73aa852cf3e0de
CCatherineeeee <48843946+CCatherineeeee@users.noreply.github.com>
2022-11-22 18:02:48 -0700

    frontend basic UI

--- examples/frontend/static/order.js
+++
@@ -1,8 +0,0 @@
-$('#all-the-buttons button').click(function (e) {
-    if ($(this).is(':disabled')) e.preventDefault();
-    else letterPress($(this));
-});
-
-var greyOutButton = function (button) {
-   button.prop('disabled', true);
-}
